ab5c175e1eae3261e6b223fc0572fa5b8aebbcdb
nicolasaboso <naboso@udesa.edu.ar>
2025-11-16 15:28:40 -0300

    HICE EL PUNTO 6 EN EL CATEGORY.JS

--- js/category.js
+++ js/category.js
@@ -1,7 +1,5 @@
 // PUNTO 2 --- HEADER Y FOOTER //
 
-
-
 let formulario = document.querySelector('.search-form');
 let inputBusqueda = document.querySelector('.search-input');
 
@@ -9,16 +7,18 @@ formulario.addEventListener('submit', function(e){
 
     e.preventDefault();
 
-    if(inputBusqueda.value === ""){
+    if (inputBusqueda.value === "") {
         return alert("El campo de búsqueda no puede estar vacío");
     } 
-    if(inputBusqueda.value.length < 3){
+
+    if (inputBusqueda.value.length < 3) {
         return alert("El término debe tener al menos 3 caracteres");
     } 
     
     localStorage.setItem("ultimaBusqueda", inputBusqueda.value);
     this.submit();
 });
+
 
 // PUNTO 3 --- MENÚ VERTICAL DE CATEGORÍAS //
 
@@ -48,76 +48,47 @@ fetch(urlCategorias)
     });
 
 
+// PUNTO 6 --- PRODUCTOS POR CATEGORÍA //
 
+let qs = location.search;
+let qsObj = new URLSearchParams(qs);
 
+let categoria = qsObj.get('category');
 
+if (categoria === null) {
+    categoria = qsObj.get('cat');
+}
 
+let tituloCategoria = document.querySelector('.category-title');
+let contenedorProductos = document.querySelector('#categoria-productos .productos');
 
+if (categoria !== null) {
 
+    tituloCategoria.innerText = 'Categoría: ' + categoria;
 
+    fetch('https://dummyjson.com/products/category/' + categoria)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
 
+            contenedorProductos.innerHTML = '';
 
+            for (let i = 0; i < data.products.length; i++) {
+                let p = data.products[i];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const tituloCat = document.querySelector('.category-title');
-const contenedorProd = document.querySelector('.productos');
-let qs = location.search;                 
-let params = new URLSearchParams(qs);     
-let cat = params.get('cat');
-if (cat === null || cat === '') {
-  if (tituloCat) {
-    tituloCat.innerText = 'Categoría no seleccionada';
-  }
-  if (contenedorProd) {
-    contenedorProd.innerHTML = '<p>No se seleccionó ninguna categoría.</p>';
-  }
-} else {
-  if (tituloCat) {
-    tituloCat.innerText = 'Categoría: ' + cat;
-  }
-  if (contenedorProd) {
-    contenedorProd.innerHTML = '<p>Cargando productos...</p>';
-  }
-  const API_BASE = 'https://dummyjson.com';
-  fetch(API_BASE + '/products/category/' + cat)
-    .then(function(respuesta) {
-      return respuesta.json();
-    })
-    .then(function(data) {
-let productos = data.products;
-if (!productos || productos.length === 0) {
-    contenedorProd.innerHTML = '<p>No hay productos en esta categoría.</p>';
-      } else {
-        let html = '';
-        let limite = 30;
-        for (let i = 0; i < limite && i < productos.length; i++) {
-        let p = productos[i];
-        html +=
-            '<div class="producto">'
-              '<img src="' + p.thumbnail + '" alt="' + p.title + '">'
-              '<h3>' + p.title + '</h3>'
-              '<p>' + p.description + '</p>'
-              '<p>Precio: $' + p.price + '</p>'
-              '<a href="./product.html?id=' + p.id + '" class="btn">Ver detalle</a>'
-              '</div>';
-        }
-        contenedorProd.innerHTML = html;
-      }
-    })
-    .catch(function(error) {
-      contenedorProd.innerHTML = '<p>Ocurrió un error al cargar la categoría.</p>';
-    });
+                contenedorProductos.innerHTML += `
+                    <article class="producto">
+                        <img src="${p.thumbnail}" alt="${p.title}">
+                        <h3>${p.title}</h3>
+                        <p>${p.description}</p>
+                        <p>$ ${p.price}</p>
+                        <a href="./product.html?id=${p.id}">Ver detalle</a>
+                    </article>
+                `;
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
